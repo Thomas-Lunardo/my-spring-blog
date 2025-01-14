ff13cf93d7b8f5eeba6349c2152842dba82aedf0
THOMAS <thomaslunardo@gmail.com>
2025-01-14 15:33:09 +0100

    feat(category): add CategoryController, Category model, and CategoryRepository to manage article categories and related operations

---
+++ src/main/java/org/wildcodeschool/myblog/repository/CategoryRepository.java
@@ -0,0 +1,16 @@
+package org.wildcodeschool.myblog.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.wildcodeschool.myblog.model.Article;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Article, Long> {
+
+    List<Article> findByTitle(String title);
+    List<Article> findByContentContaining(String content);
+    List<Article> findByCreatedAtGreaterThan(LocalDateTime createdAt);
+    List<Article> findFirst5ByCreatedAtBeforeOrderByCreatedAtDesc(LocalDateTime createdAt);
+
+}
